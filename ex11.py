0e62760a238fe67670bca620d6de49edeec0de38
Atkins <spencer.atkins@dbschenker.com>
2018-02-05 18:07:32 +0000

    LearnPythonTHW manual and sample ex11 file

---
+++ ex11.py
@@ -0,0 +1,13 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+#another use for raw_input
+print "Hi there, do you want to play a game? (Y/N)"
+game = raw_input()
+print "Either way, I haven't programmed one yet. :-)"
